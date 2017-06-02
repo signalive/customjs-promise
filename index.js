51c991a5d1d64d3c39d6f19154b28da970d980b8
Mert Dogar <mertdogar@gmail.com>
2017-06-02 14:46:08 +0300

    :bug: Fix referencing the original customjs api

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ var Custom = {
 };
 var instance = new OriginalCustom();
 
-Custom.Configuration.changePassword = utils.promisify(instance, 'changePassword');
-Custom.Configuration.getKAM = utils.promisify(instance, 'getKAM');
-Custom.Configuration.getNativePortraitMode = utils.promisify(instance, 'getNativePortraitMode');
-Custom.Configuration.getPowerOnOffHistory = utils.promisify(instance, 'getPowerOnOffHistory');
-Custom.Configuration.getPowerOnStatus = utils.promisify(instance, 'getPowerOnStatus');
-Custom.Configuration.getWoWLAN = utils.promisify(instance, 'getWoWLAN');
-Custom.Configuration.setKAM = utils.promisify(instance, 'setKAM');
-Custom.Configuration.setNativePortraitMode = utils.promisify(instance, 'setNativePortraitMode');
-Custom.Configuration.setPowerOnStatus = utils.promisify(instance, 'setPowerOnStatus');
-Custom.Configuration.setWoWLAN = utils.promisify(instance, 'setWoWLAN');
+Custom.Configuration.changePassword = utils.promisify(instance.Configuration, 'changePassword');
+Custom.Configuration.getKAM = utils.promisify(instance.Configuration, 'getKAM');
+Custom.Configuration.getNativePortraitMode = utils.promisify(instance.Configuration, 'getNativePortraitMode');
+Custom.Configuration.getPowerOnOffHistory = utils.promisify(instance.Configuration, 'getPowerOnOffHistory');
+Custom.Configuration.getPowerOnStatus = utils.promisify(instance.Configuration, 'getPowerOnStatus');
+Custom.Configuration.getWoWLAN = utils.promisify(instance.Configuration, 'getWoWLAN');
+Custom.Configuration.setKAM = utils.promisify(instance.Configuration, 'setKAM');
+Custom.Configuration.setNativePortraitMode = utils.promisify(instance.Configuration, 'setNativePortraitMode');
+Custom.Configuration.setPowerOnStatus = utils.promisify(instance.Configuration, 'setPowerOnStatus');
+Custom.Configuration.setWoWLAN = utils.promisify(instance.Configuration, 'setWoWLAN');
 
-Custom.Signage.disableApplication = utils.promisify(instance, 'disableApplication');
-Custom.Signage.getApplicationInfo = utils.promisify(instance, 'getApplicationInfo');
-Custom.Signage.getwebOSVersion = utils.promisify(instance, 'getwebOSVersion');
-Custom.Signage.switchApplication = utils.promisify(instance, 'switchApplication');
+Custom.Signage.disableApplication = utils.promisify(instance.Signage, 'disableApplication');
+Custom.Signage.getApplicationInfo = utils.promisify(instance.Signage, 'getApplicationInfo');
+Custom.Signage.getwebOSVersion = utils.promisify(instance.Signage, 'getwebOSVersion');
+Custom.Signage.switchApplication = utils.promisify(instance.Signage, 'switchApplication');
 
-Custom.VideoSync.setMaster = utils.promisify(instance, 'setMaster');
-Custom.VideoSync.setSlave = utils.promisify(instance, 'setSlave');
+Custom.VideoSync.setMaster = utils.promisify(instance.VideoSync, 'setMaster');
+Custom.VideoSync.setSlave = utils.promisify(instance.VideoSync, 'setSlave');
 
 
 module.exports = Custom;
